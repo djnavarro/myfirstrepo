793e4d7cbd0f389ca286f8e20c7aa9b9d2791c1d
Bojana-Popovic <bojana.popovic@student.unsw.edu.au>
2020-03-12 02:43:50 +0000

    create setup

---
+++ setup.R
@@ -0,0 +1,9 @@
+# Set up script ie allow tidyverse to work; packages need to be installed on the machine before you can use them
+
+install.packages("tidyverse")
+install.packages("usethis")
+install.packages("remotes")
+
+#in git pane see the yellow question marks; means git is keeping track of all of the changes being made to the file
+
+#to register the changes made here for github to be aware of them; click on the setup.R file in the git pane (means you want to add this to github)
