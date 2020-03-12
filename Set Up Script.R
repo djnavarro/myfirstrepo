864e250f145cb07b055c86c1811102122b43d6aa
z5164942 <z5164942@ad.unsw.edu.au>
2020-03-12 02:42:07 +0000

    Introduce Myself

---
+++ Set Up Script.R
@@ -0,0 +1,10 @@
+# setting up R 
+# this is a local version of the github one
+
+# installing pacakages before you can use them 
+install.packages("tidyverse")
+install.packages("usethis")
+install.packages("remotes")
+
+library(tidyverse)
+
--- git_introduction.R
+++ git_introduction.R
@@ -1,5 +1,7 @@
 # send commands to the terminal
 system(
-  'git config --global user.email "YOUR USER NAME HERE";
-   git config --global user.name "YOUR NAME HERE";'
+  'git config --global user.email "z5164942@ad.unsw.edu.au";
+   git config --global user.name "z5164942";'
 )
+
+# minimum info git needs to know to interact with Rstudio 
