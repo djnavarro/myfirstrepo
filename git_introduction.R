9dcecc7b2282bea6d14722c09ff65025e0698d77
cathyj-lin <z5117580@ad.unsw.edu.au>
2020-03-12 02:43:19 +0000

    Introduce yourself

--- git_introduction.R
+++ git_introduction.R
@@ -1,5 +1,7 @@
 # send commands to the terminal
 system(
-  'git config --global user.email "YOUR USER NAME HERE";
-   git config --global user.name "YOUR NAME HERE";'
+  'git config --global user.email "z5117580@ad.unsw.edu.au";
+   git config --global user.name "cathyj-lin";'
 )
+
+#run it 
